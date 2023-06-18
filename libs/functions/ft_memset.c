77e4d01957b64928aa23471d19f2229319dcdf39
kaykesandes <kaykegy@proton.me>
2023-06-18 11:27:22 -0300

    memset_ok

--- libs/functions/ft_memset.c
+++ libs/functions/ft_memset.c
@@ -6,44 +6,38 @@
 /*   By: kgoncalv <kaykegy@proton.me>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 18:14:39 by kgoncalv          #+#    #+#             */
-/*   Updated: 2023/06/17 18:32:53 by kgoncalv         ###   ########.fr       */
+/*   Updated: 2023/06/18 11:27:02 by kgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-
-
 void *ft_memset(void *ptr, int value, size_t num)
 {
-    while (num <= 0)
+    unsigned char *p = (unsigned char *)ptr; // Conversão do ponteiro para o tipo apropriado
+
+    while (num > 0)
     {
-        *ptr[num] = value;
-        
+        *p = (unsigned char)value; // Atribuição do valor ao byte atual
+        p++; // Avançar para o próximo byte
+        num--; // Decrementar o número de bytes restantes
     }
+    return ptr; // Retornar o ponteiro para o bloco de memória preenchido
 }
 
-int main() 
-{
-    int array[5];
-    size_t num_elements = sizeof(array) / sizeof(int);
-
-    memset(array, 0, sizeof(array));
-
-    for (size_t i = 0; i < num_elements; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    int ft_array[5];
-    size_t ft_num_elements = sizeof(ft_array) / sizeof(int);
-
-    ft_memset(ft_array, 0, sizeof(ft_array));
-
-    for (size_t i = 0; i < num_elements; i++) {
-        printf("%d ", ft_array[i]);
-    }
-    printf("\n");
-
-    return 0;
-}
+// int main() 
+// {
+//     int array[5];
+//     size_t num_elements = sizeof(array) / sizeof(int);
+//     memset(array, 0, sizeof(array));
+//     for (size_t i = 0; i < num_elements; i++) {
+//         printf("%d ", array[i]);
+//     }
+//     printf("\n");
+//     int ft_array[5];
+//     size_t ft_num_elements = sizeof(ft_array) / sizeof(int);
+//     ft_memset(ft_array, 0, sizeof(ft_array));
+//     for (size_t i = 0; i < num_elements; i++) {
+//         printf("%d ", ft_array[i]);
+//     }
+//     printf("\n");
+//     return 0;
+// }
