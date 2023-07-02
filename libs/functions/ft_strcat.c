571f8cd3fd2f54da002a651669a408cc2768e188
kaykegy@proton.me <kaykegy@proton.me>
2023-07-02 02:22:11 -0300

    strcat

---
+++ libs/functions/ft_strcat.c
@@ -0,0 +1,24 @@
+char *ft_strcat(char *dest, const char *src) {
+    unsigned int i = 0;
+
+    while (dest[i] != '\0')
+        i++;
+
+    while ((dest[i++] = *src++));
+
+    return dest;
+}
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+//     char dest[20] = "Hello, ";
+//     const char *src = "World!";
+
+//     ft_strcat(dest, src);
+
+//     printf("String concatenada: %s\n", dest);
+
+//     return 0;
+// }
