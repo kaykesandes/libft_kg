521339985cef2a98e5385bb23d9f4761b5e10c53
kaykegy@proton.me <kaykegy@proton.me>
2023-07-02 01:03:49 -0300

    strdup

---
+++ libs/functions/ft_strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+// #include <stdio.h>
+char *ft_strdup(const char *str)
+{
+    unsigned int i = 0;
+    unsigned int leng = 0;
+
+    while (str[leng] != '\0')
+        leng++;
+    
+    char* ret = malloc(leng + 1);
+    while (i <= leng)
+    {
+        ret[i] = str[i];
+        i++;
+    }
+    return (ret);
+}
+
+// int main(void) {
+//     const char *original = "Hello, World!";
+//     char *duplicate = ft_strdup(original);
+
+//     printf("String original:  %s\n", original);
+//     printf("String duplicada: %s\n", duplicate);
+
+//     free(duplicate); // Liberar a memória alocada pela strdup
+
+//     return 0;
+// }
