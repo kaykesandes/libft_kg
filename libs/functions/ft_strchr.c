69494f4d13043c7ee70adc6658fd08d7b5ebbe02
kaykegy@proton.me <kaykegy@proton.me>
2023-07-02 02:59:59 -0300

    strchr

---
+++ libs/functions/ft_strchr.c
@@ -0,0 +1,29 @@
+// #include <stdio.h>
+#include <string.h>
+char    *ft_strchr(const char *str, int c)
+{
+    while (*str != '\0')
+    {
+        if (*str == c)
+            return (char *)str;
+        str++;
+    }
+    if (*str == c)
+        return (char *) str;
+
+    return NULL;
+}
+
+
+// int main(void)
+// {
+//     const char *str = "Ola mundo";
+//     char chart = 'm';
+
+//     char *result = ft_strchr(str, chart);
+
+//     if (result != NULL)
+//         printf("Encontrado %ld \n", result - str);
+//     else
+//         printf("N encontrado\n");
+// }
