643720ea9c2e9a82f3fa89990cc6e72af7abe352
kaykegy@proton.me <kaykegy@proton.me>
2023-07-02 00:10:12 -0300

    strlen

---
+++ libs/functions/ft_strlen.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+// #include <stdio.h>
+
+size_t ft_strlen(const char *str)
+{
+    unsigned int i = 0;
+
+    while (str[i] != '\0')
+        i++;
+    return i;
+}
+
+// int main(void)
+// {
+//     char test[] = "012345";
+//     printf("%d", ft_strlen(test));
+// }
