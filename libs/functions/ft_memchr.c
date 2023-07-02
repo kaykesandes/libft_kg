7610769280f9627acf7fe69fe84c1c1e8cda3039
kaykegy@proton.me <kaykegy@proton.me>
2023-07-01 23:58:49 -0300

    memchr memcmp

---
+++ libs/functions/ft_memchr.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+// #include <stdint.h>
+// #include <stdio.h>
+void* ft_memchr(const void *ptr, int value, size_t num)
+{
+    const unsigned char *p = ptr;
+    unsigned char val = (unsigned char)value;
+    while(num--)
+    {
+        if(p[num] == val)
+            return ((void* )&p[num]);
+    }
+    return NULL;
+}
+
+// int main(void)
+// {
+//     char test[] = "Ola mundo";
+
+//     printf("%x", ft_memchr(test, ' ', sizeof(test)));
+// }
---
+++ libs/functions/ft_memcmp.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+//#include <stdio.h>
+
+int ft_memcmp(const void *ptr1, const void *ptr2, size_t num)
+{
+    const unsigned char *p1 = ptr1;
+    const unsigned char *p2 = ptr2;
+
+    while (num--) 
+    {
+        if(p1[num] > p2[num])
+            return 1;
+        else if (p1[num] < p2[num])
+            return -1;
+    }
+    return 0;
+}
+
+// int main(void)
+// {
+//     unsigned char buffer1[] = {0x10, 0x20, 0x30, 0x40, 0x50};
+//     unsigned char buffer2[] = {0x10, 0x20, 0x30, 0x40, 0x50};
+
+//     int result = ft_memcmp(buffer1, buffer2, sizeof(buffer1));
+
+//     printf("%d", result);
+// }
