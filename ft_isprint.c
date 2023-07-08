e0b8945fbb186a3a52433d11f3147073611bc390
kaykegy@proton.me <kaykegy@proton.me>
2023-07-08 19:48:34 -0300

    isprint_revature

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,11 +1,27 @@
 #include "libft.h"
+#include <ctype.h>
+#include <stdio.h>
 
 int ft_isprint(int c)
 {
-    if (c <= 32 && c >= 126)
+    if (c >= 32 && c <= 126)
         return 1;
 
     return 0;
 }
 
+
+// int main(void)
+// {
+//     char c;
+    
+//     // c = 'c';
+//     // printf("%c == %d", c, ft_isprint(c));
+//     // c = '\n';
+//     // printf("%c == %d", c, ft_isprint(c));
+//     for (int c = 1; c <=127; c++) 
+//         if (isprint(c) != 0)
+//             printf("%c", c);
+//     return 0;
+// }
 
